11c2a3b5de7c569da1c15dd456fadcbeae612ecf
Kęstutis Skrebė <kestutis.skrebe@stud.viko.lt>
2026-06-07 14:48:40 +0300

    Add caching support to CarServiceApplication and include spring-boot-starter-cache dependency

--- src/main/java/lt/viko/eif/kskrebe/carservice/CarServiceApplication.java
+++ src/main/java/lt/viko/eif/kskrebe/carservice/CarServiceApplication.java
@@ -2,7 +2,12 @@ package lt.viko.eif.kskrebe.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * Main Spring Boot application class for the Car Service REST API.
+ */
+@EnableCaching //ieško pažymėtų @Cacheable, @CachePut ar @CacheEvict
 @SpringBootApplication
 public class CarServiceApplication {
 
